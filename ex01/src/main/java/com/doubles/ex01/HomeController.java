e00819cd68f67a5dd6591113c32e6d9afc8e2ee1
DESKTOP-0LTGVPG\walba <walbatrossw@gmail.com>
2017-11-26 23:07:16 +0900

    7th commit - 기본적인 기능의 게시물 관리를 위한 ex01 프로젝트 생성 및 스프링 설정 완료

---
+++ ex01/src/main/java/com/doubles/ex01/HomeController.java
@@ -0,0 +1,14 @@
+package com.doubles.ex01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+
+}
---
+++ ex01/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Ex01 Home</title>
+</head>
+<body>
+    <h2>Hello World</h2>
+    <p>This is home</p>
+</body>
+</html>
