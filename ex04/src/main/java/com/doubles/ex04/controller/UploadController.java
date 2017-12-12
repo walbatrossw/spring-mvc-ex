98361b217b0b1761cc401db21eaf10f3ae6ae459
DESKTOP-0LTGVPG\walba <walbatrossw@gmail.com>
2017-12-12 13:08:53 +0900

    61st commit - 게시판 첨부파일 기능구현3 : 저장 경로 설정, 업로드 파일 저장

--- ex04/src/main/java/com/doubles/ex04/controller/UploadController.java
+++ ex04/src/main/java/com/doubles/ex04/controller/UploadController.java
@@ -4,15 +4,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.io.File;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/fileupload")
 public class UploadController {
 
     private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
+
+    // 파일 저장 경로
+    @Resource(name = "uploadPath")
+    private String uploadPath;
+
 
     // 파일 업로드 페이지
     @RequestMapping(value = "/uploadForm", method = RequestMethod.GET)
@@ -29,7 +39,19 @@ public class UploadController {
         logger.info("contentType : " + file.getContentType());
         logger.info("============================================================");
 
+        String savedName = uploadFile(file.getOriginalFilename(), file.getBytes());
+        model.addAttribute("savedName", savedName);
+
         return "/fileupload/upload_form";
+    }
+
+    private String uploadFile(String originalFileName, byte[] fileData) throws Exception {
+
+        UUID uuid = UUID.randomUUID();
+        String savedName = uuid.toString() + "_" + originalFileName;
+        File target = new File(uploadPath, savedName);
+        FileCopyUtils.copy(fileData, target);
+        return savedName;
     }
 
 }
