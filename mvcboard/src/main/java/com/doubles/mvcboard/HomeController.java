7998c1bfbca8d3102411af40e96ba4715fe3080f
SangSeob Yoon <walbatrossw@gmail.com>
2018-02-22 22:22:01 +0900

    111st commit : 게시판 복습예제 생성

---
+++ mvcboard/src/main/java/com/doubles/mvcboard/HomeController.java
@@ -0,0 +1,15 @@
+package com.doubles.mvcboard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(Model model) {
+        model.addAttribute("greeting", "hello World!");
+        return "home";
+    }
+}
---
+++ mvcboard/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+
+<h2>This is welcome page....</h2>
+
+<p>${greeting}</p>
+
+</body>
+</html>
