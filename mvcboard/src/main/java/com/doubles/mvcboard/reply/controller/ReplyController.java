8ea97d3577a9b09c1799a268f054318b4e3443f4
SangSeob Yoon <walbatrossw@gmail.com>
2018-03-09 22:40:35 +0900

    127th commit : 복습 14 - 댓글처리 컨트롤러 구현완료

--- mvcboard/src/main/java/com/doubles/mvcboard/reply/controller/ReplyController.java
+++ mvcboard/src/main/java/com/doubles/mvcboard/reply/controller/ReplyController.java
@@ -1,10 +1,13 @@
 package com.doubles.mvcboard.reply.controller;
 
+import com.doubles.mvcboard.reply.domain.ReplyVO;
 import com.doubles.mvcboard.reply.service.ReplyService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
+import java.util.List;
 
 @RestController
 @RequestMapping("/replies")
@@ -17,4 +20,55 @@ public class ReplyController {
         this.replyService = replyService;
     }
 
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity<String> register(@RequestBody ReplyVO replyVO) {
+        ResponseEntity<String> entity = null;
+        try {
+            replyService.create(replyVO);
+            entity = new ResponseEntity<>("regSuccess", HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            entity = new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return entity;
+    }
+
+    @RequestMapping(value = "/all/{articleNo}", method = RequestMethod.GET)
+    public ResponseEntity<List<ReplyVO>> list(@PathVariable("articleNo") Integer articleNo) {
+        ResponseEntity<List<ReplyVO>> entity = null;
+        try {
+            entity = new ResponseEntity<>(replyService.list(articleNo), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            entity = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return entity;
+    }
+
+    @RequestMapping(value = "/{replyNo}", method = {RequestMethod.PUT, RequestMethod.PATCH})
+    public ResponseEntity<String> update(@PathVariable("replyNo") Integer replyNo, @RequestBody ReplyVO replyVO) {
+        ResponseEntity<String> entity = null;
+        try {
+            replyVO.setReplyNo(replyNo);
+            replyService.update(replyVO);
+            entity = new ResponseEntity<>("modSuccess", HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            entity = new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return entity;
+    }
+
+    @RequestMapping(value = "/{replyNo}", method = RequestMethod.DELETE)
+    public ResponseEntity<String> delete(@PathVariable("replyNo") Integer replyNo) {
+        ResponseEntity<String> entity = null;
+        try {
+            replyService.delete(replyNo);
+            entity = new ResponseEntity<>("delSuccess", HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            entity = new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return entity;
+    }
 }
